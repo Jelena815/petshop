e51da7f8ff5ec53a17f6b54844be0f227a2ff085
Jelena815 <serjelena9@gmail.com>
2024-01-18 21:09:41 -0800

    Added navbar
    Added navbar and template pages Proizvodi, Galerija, O nama

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GalerijaComponent } from './sadrzaj/galerija/galerija.component';
+import { ONamaComponent } from './sadrzaj/o-nama/o-nama.component';
+import { ProizvodiComponent } from './sadrzaj/proizvodi/proizvodi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'galerija', component: GalerijaComponent},
+  {path: 'o-nama', component: ONamaComponent},
+  {path: 'proizvodi', component: ProizvodiComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
